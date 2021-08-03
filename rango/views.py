e9606029ac3bcfd6117da96b5e20236455ca36ff
Reema <rboualayan@gmail.com>
2021-08-03 14:17:54 +0300

    likes in progress, jquery/ajax added to be updated

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.views.generic import View
+from django.utils.decorators import method_decorator
 
 from rango.models import Category, Page, Comment
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm, CommentForm
@@ -194,3 +196,19 @@ def visitor_cookie_handler(request): #obtain number of visits to the site
         request.session['last_visit'] = last_visit_cookie #set last visit cookie
 
     request.session['visits'] = visits #update/set visits cookie
+
+
+class LikeCategoryView(View): 
+    @method_decorator(login_required)
+    def get(self, request):
+        category_id = request.GET['category_id'] 
+        try:
+            category = Category.objects.get(id=int(category_id)) 
+        except Category.DoesNotExist:
+            return HttpResponse(-1) 
+        except ValueError:
+            return HttpResponse(-1) 
+        
+        category.likes = category.likes + 1
+        category.save()
+        return HttpResponse(category.likes)
