14f69825fd5d6761810b7fcffaa086abce27bdf7
Liam Fotheringham <2595981F@student.gla.ac.uk>
2021-08-04 10:12:02 +0100

    rango-ajax.js - add comments

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -6,16 +6,20 @@
  * 
  */
 $(document).ready(function(){
+    //Whenever user types in Username input
     $("#registerUsername").on('keyup',function(){
         var query;
-        query = $(this).val();
+        query = $(this).val(); //Attach input value to query variable
     
+        //Send get request to /rango/check_username
         $.get('/rango/check_username/',
         {'username': query},
         function(data){
+            //If the username already exists - inform user
             if(data == 'True'){
                 $('#username-exists-span').text('This username already exists')
             }
+            //Otheriwse, tell user it is available
             else{
                 $('#username-exists-span').text('This username is not taken')
             }
